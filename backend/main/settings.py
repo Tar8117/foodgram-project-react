d8cfd38c1993913ce0111583da1486311f36c7f7
Oliver <tar8117@yahoo.com>
2022-04-06 05:40:39 +0300

    first

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -81,7 +81,7 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
